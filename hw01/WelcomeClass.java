e20f97c0d9d1016af10c4da8f18b8918d8b34504
dchangeux <dmc418@lehigh.edu>
2015-01-27 02:19:39 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,31 @@
+//Danelia Changeux 
+//Homework 1
+//Welcome Class Java Program 
+//
+//  first compile the program
+//      javac WelcomeClass.Java
+//  run the program
+//      Java WelcomeClass//
+
+
+//  define a class
+public class WelcomeClass{
+
+//  add main method 
+  public static void main(String[ ] args) {
+      
+// print the statement with the box around it 
+   System.out.println("-----------");
+   System.out.println("| WELCOME |");
+   System.out.println("-----------");
+   System.out.println("  ^  ^  ^  ^  ^  ^");
+   System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+   System.out.println("<-D--M--C--4--1--8->");
+   System.out.println(" \\/ \\/ \\/ \\/ \\/ \\ /");
+   System.out.println("  v  v  v  v  v  v");
+   System.out.println("My full name is Danelia Monica Maria Changeux and my name comes from my Hatain and Salvadorean background");
+   System.out.println("test");
+   
+   //test/testtest
+  }
+}
